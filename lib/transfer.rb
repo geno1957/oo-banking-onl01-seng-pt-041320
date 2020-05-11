d33dd6096608f0d95011158021373d6124c988bc
Jared  E <jared.epps322@gmail.com>
2020-05-11 21:41:44 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,16 +13,16 @@ class Transfer
     @sender.valid? && @receiver.valid? ? true : false
   end
   
-  # def execute_transaction
-  #   if @sender.balance > @amount && @status == "pending"
-  #     @sender.balance -= @amount 
-  #     @receiver.balance += @amount 
-  #     @status = "complete"
-  #   else 
-  #     @status = "rejected"
-  #     return "Transaction rejected. Please check your account balance."
-  #   end 
-  # end 
+  def execute_transaction
+    if @sender.balance > @amount && @status == "pending"
+      @sender.balance -= @amount 
+      @receiver.balance += @amount 
+      @status = "complete"
+    else 
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    end 
+  end 
   
   
   
