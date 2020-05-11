b317d99d2ed62a6be1ac8869638061715010e5d9
Jared  E <jared.epps322@gmail.com>
2020-05-11 22:00:39 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -30,7 +30,7 @@ class Transfer
       @receiver.balance += @amount 
       @status = "complete"
       elsif
-      @sender.balance < @amount && 
+      @sender.balance < @amount && @status
     
   
   
